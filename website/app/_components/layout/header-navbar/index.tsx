0145941ce2b55941dc46895ff906c108852a6d3b
Guillem Aldeguer Lima <guillem.aldeguer@gmail.com>
2026-06-05 00:50:03 +0200

    chore: update url

--- website/app/_components/layout/header-navbar/index.tsx
+++ website/app/_components/layout/header-navbar/index.tsx
@@ -1,6 +1,6 @@
 import Image from 'next/image';
 
-import { REPOSITORY_URL } from '../../../constants';
+import { isProduction, REPOSITORY_URL } from '../../../constants';
 import { NavLinks } from './NavLinks';
 import { ThemeSwitch } from './ThemeSwitch';
 
@@ -10,7 +10,13 @@ export const HeaderNavbar = () => (
   <nav className={styles.headerNavbar}>
     <div className={styles.logoContainer}>
       <a className={styles.anchor} href="/">
-        <Image alt="logo" height={45} priority src="/jarl.png" width={45} />
+        <Image
+          alt="logo"
+          height={45}
+          priority
+          src={isProduction ? 'https://naimikan.github.io/jarl/jarl.png' : '/jarl.png'}
+          width={45}
+        />
       </a>
     </div>
 
