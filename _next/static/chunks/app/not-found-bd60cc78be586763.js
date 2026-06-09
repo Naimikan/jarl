0e00688c37f2cfba33010195a6be7782de40840a
Naimikan <Naimikan@users.noreply.github.com>
2026-06-09 20:44:57 +0000

    deploy: 2011d1a672dcdea95bef769f0d2f391be4f9adc4

---
+++ _next/static/chunks/app/not-found-bd60cc78be586763.js
@@ -0,0 +1,1 @@
+(self.webpackChunk_N_E=self.webpackChunk_N_E||[]).push([[345],{6894:(e,s,_)=>{Promise.resolve().then(_.t.bind(_,9121,23))}},e=>{e.O(0,[121,73,394,358],()=>e(e.s=6894)),_N_E=e.O()}]);
